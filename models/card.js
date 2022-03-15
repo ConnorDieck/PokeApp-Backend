7846a9d72e25ca35e256c95e18274f008ec2ca4b
Connor Dieck <connordieck@gmail.com>
2022-03-15 18:15:34 -0400

    started creating methods on card model

--- models/card.js
+++ models/card.js
@@ -8,12 +8,33 @@ const { setPokemonBase } = require("../helpers/transform");
 /** Related functions for pokemon */
 
 class Card {
-	constructor(id, pokemonId, name, gender, type1, type2) {
+	constructor(id, nickname, gender, nature, ability, art, species, item) {
 		(this.id = id),
-			(this.pokemonId = pokemonId),
-			(this.name = name),
+			(this.nickname = nickname),
 			(this.gender = gender),
-			(this.type1 = type1),
-			(this.type2 = type2);
+			(this.nature = nature),
+			(this.ability = ability),
+			(this.art = art),
+			(this.species = species),
+			(this.item = item);
+	}
+
+	// Gets all cards built by given user
+	async getAll(userId) {
+		const cardsRes = await db.query(
+			`SELECT id,
+						nickname,
+						gender,
+						nature,
+						ability,
+						art,
+						species,
+						item
+				FROM cards c
+					LEFT JOIN users_cards AS uc ON c.id = uc.card_id
+					LEFT JOIN users AS u ON uc.user_id = u.id
+				WHERE uc.id = $1`,
+			[ userId ]
+		);
 	}
 }
