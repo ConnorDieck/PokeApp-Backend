66b248eccc9931770e4c5040934d16b30f4eca2f
Connor Dieck <connordieck@gmail.com>
2022-03-24 17:20:59 -0400

    added tests for and fixed addCard() method

--- models/team.js
+++ models/team.js
@@ -17,33 +17,127 @@ class Team {
 			[ username ]
 		);
 
-		if (!teamsRes.rows[0]) throw new NotFoundError("No Pokemon built yet");
+		if (!teamsRes.rows[0]) throw new NotFoundError("No teams built yet");
 
 		return teamsRes.rows;
 	}
 
-	/** Returns details of one card given card's id */
-	static async get(cardId) {
-		const res = await db.query(
-			`SELECT id, nickname, art, username, gender, nature_id AS "natureId", ability_id AS "abilityId", art, species_id AS "speciesId", item_id AS "itemId"
-			 FROM cards
+	/** Returns details of one team given team's id */
+	static async get(teamId) {
+		const teamRes = await db.query(
+			`SELECT id, name, username
+			 FROM teams
 			 WHERE id = $1`,
-			[ cardId ]
+			[ teamId ]
 		);
 
-		if (!res.rows[0]) throw new NotFoundError(`No card with given id: ${cardId}`);
+		if (!teamRes.rows[0]) throw new NotFoundError(`No team with given id: ${teamId}`);
 
-		const cardRes = res.rows[0];
+		const team = teamRes.rows[0];
 
-		/** Get associated moves and add to returned card object */
-		const moveIds = await Move.getAllFromCard(cardId);
+		/** Get associated card ids, nicknames, and art to load into return object */
+		const result = await db.query(
+			`SELECT tc.card_id AS "cardId", c.nickname, c.art
+             FROM teams_cards tc
+                LEFT JOIN cards c ON tc.card_id = c.id
+             WHERE tc.team_id = $1`,
+			[ teamId ]
+		);
 
-		const card = {
-			...cardRes,
-			moveIds
+		const teamCards = result.rows;
+
+		const build = {
+			...team,
+			teamCards
 		};
 
-		return card;
+		return build;
+	}
+
+	/** Creates a new team and adds it to the db */
+	static async create(name, username) {
+		const duplicateCheck = await db.query(
+			`SELECT name
+			 FROM teams
+			 WHERE name = $1 AND username = $2`,
+			[ name, username ]
+		);
+
+		if (duplicateCheck.rows[0]) throw new BadRequestError(`Duplicate team: ${name}`);
+
+		const result = await db.query(
+			`INSERT INTO teams (name, username)
+             VALUES ($1, $2)
+             RETURNING id, name, username`,
+			[ name, username ]
+		);
+
+		const newTeam = result.rows[0];
+
+		return newTeam;
+	}
+
+	/** Adds an existing card to an existing team */
+	static async addCard(teamId, cardId, username) {
+		const ownerCheck = await db.query(
+			`SELECT name
+			 FROM teams
+			 WHERE id = $1 AND username = $2`,
+			[ teamId, username ]
+		);
+
+		const team = ownerCheck.rows[0];
+
+		if (!team) throw new NotFoundError(`No team owned with given id: ${teamId}`);
+
+		const result = await db.query(
+			`INSERT INTO teams_cards (team_id, card_id)
+             VALUES ($1, $2)
+             RETURNING team_id AS "teamId", card_id AS "cardId"`,
+			[ teamId, cardId ]
+		);
+
+		const teamCard = result.rows[0];
+		return teamCard;
+	}
+
+	/** Removes a card from an existing team */
+	static async removeCard(teamId, cardId, username) {
+		const ownerCheck = await db.query(
+			`SELECT name
+			 FROM teams
+			 WHERE id = $1 AND username = $2`,
+			[ teamId, username ]
+		);
+
+		const team = ownerCheck.rows[0];
+
+		if (!team) throw new NotFoundError(`No team found with given id: ${teamId}`);
+
+		const result = await db.query(
+			`DELETE
+             FROM teams_cards
+             WHERE team_id = $1 AND card_id = $2
+             RETURNING team_id`,
+			[ teamId, cardId ]
+		);
+
+		const deleted = result.rows[0];
+		if (!deleted) throw new NotFoundError(`No card ${cardId} on team ${teamId}`);
+	}
+
+	/** Removes team from db */
+	static async remove(teamId, username) {
+		const result = await db.query(
+			`DELETE
+             FROM teams
+             WHERE team_id = $1 AND username = $2
+             RETURNING team_id`,
+			[ teamId, username ]
+		);
+
+		const deleted = result.rows[0];
+		if (!deleted) throw new NotFoundError(`No team found with given id: ${teamId}`);
 	}
 }
 
---
+++ models/team.test.js
@@ -0,0 +1,182 @@
+"use strict";
+
+const db = require("../db.js");
+const { BadRequestError, NotFoundError } = require("../expressError");
+const Team = require("./team.js");
+const Card = require("./card.js");
+const {
+	commonBeforeAll,
+	commonBeforeEach,
+	commonAfterEach,
+	commonAfterAll,
+	testAbilityIds,
+	testItemIds,
+	testMoveIds,
+	testNatureIds,
+	testTeamIds,
+	testUsernames,
+	testCardIds
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** getAll */
+
+describe("getAll", function() {
+	test("works", async function() {
+		const teams = await Team.getAll(testUsernames[0]);
+
+		expect(teams).toEqual([
+			{
+				id       : expect.any(Number),
+				name     : "u1team",
+				username : testUsernames[0]
+			}
+		]);
+	});
+
+	test("returns not found error if no teams exist with given username", async function() {
+		try {
+			await Team.getAll("noteams");
+			fail();
+		} catch (err) {
+			expect(err instanceof NotFoundError).toBeTruthy();
+		}
+	});
+});
+
+/************************************** get */
+
+describe("get", function() {
+	test("works", async function() {
+		const team = await Team.get(testTeamIds[0], testUsernames[0]);
+
+		expect(team).toEqual({
+			id        : expect.any(Number),
+			name      : "u1team",
+			username  : testUsernames[0],
+			teamCards : []
+		});
+	});
+
+	test("correctly pulls card details if exist", async function() {
+		const testCard = {
+			nickname  : "Spicy",
+			gender    : true,
+			art       :
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/257.png",
+			natureId  : testNatureIds[0],
+			abilityId : testAbilityIds[0],
+			speciesId : 257,
+			itemId    : testItemIds[0],
+			moveIds   : testMoveIds.slice(0, 4)
+		};
+
+		const card = await Card.create(testCard);
+
+		await db.query(
+			`INSERT INTO teams_cards
+             (team_id, card_id)
+             VALUES ($1, $2)`,
+			[ testTeamIds[0], card.id ]
+		);
+
+		const team = await Team.get(testTeamIds[0], testUsernames[0]);
+
+		expect(team).toEqual({
+			id        : expect.any(Number),
+			name      : "u1team",
+			username  : testUsernames[0],
+			teamCards : [
+				{
+					cardId   : expect.any(Number),
+					nickname : "Spicy",
+					art      :
+						"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/257.png"
+				}
+			]
+		});
+	});
+
+	test("returns not found error if no teams exist with given username", async function() {
+		try {
+			await Team.get(999, "noteams");
+			fail();
+		} catch (err) {
+			expect(err instanceof NotFoundError).toBeTruthy();
+		}
+	});
+});
+
+/************************************** create */
+
+describe("create", function() {
+	test("works", async function() {
+		const newTeam = await Team.create("testTeam", testUsernames[0]);
+
+		const result = await db.query(
+			`SELECT id, name, username
+             FROM teams
+             WHERE id = $1`,
+			[ newTeam.id ]
+		);
+		expect(result.rows[0]).toEqual(newTeam);
+	});
+
+	test("returns bad request error if duplicate", async function() {
+		try {
+			await Team.create("testTeam", testUsernames[0]);
+			await Team.create("testTeam", testUsernames[0]);
+			fail();
+		} catch (err) {
+			expect(err instanceof BadRequestError).toBeTruthy();
+		}
+	});
+});
+
+/************************************** addCard */
+
+describe("addCard", function() {
+	test("works", async function() {
+		const testCard = {
+			nickname  : "Spicy",
+			gender    : true,
+			art       :
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/257.png",
+			natureId  : testNatureIds[0],
+			abilityId : testAbilityIds[0],
+			speciesId : 257,
+			itemId    : testItemIds[0],
+			moveIds   : testMoveIds.slice(0, 4)
+		};
+		const card = await Card.create(testCard, testUsernames[0]);
+
+		const teamCard = await Team.addCard(testTeamIds[0], card.id, testUsernames[0]);
+
+		console.log(teamCard);
+
+		expect(teamCard.teamId).toEqual(testTeamIds[0]);
+		expect(teamCard.cardId).toEqual(card.id);
+
+		const result = await db.query(
+			`SELECT team_id, card_id
+             FROM teams_cards
+             WHERE team_id = $1 AND card_id = $2`,
+			[ testTeamIds[0], card.id ]
+		);
+
+		expect(result.rows.length).not.toEqual(0);
+	});
+
+	test("returns not found error if no teams exist with given username", async function() {
+		try {
+			await Team.addCard(999, 1, testUsernames[0]);
+			fail();
+		} catch (err) {
+			expect(err instanceof NotFoundError).toBeTruthy();
+		}
+	});
+});
